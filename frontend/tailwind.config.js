e64a6481b8b355287f0d19de0fcac04c875e4d67
Nikhilsingh0408 <148408596+Nikhilsingh0408@users.noreply.github.com>
2024-12-10 18:16:28 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,8 @@
-export default {
-  content: ["./src/**/*.{html,js}"],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}", // Adjust this based on your project structure
+    "./public/index.html",             // Include your HTML files if applicable
+  ],
   theme: {
     extend: {},
   },
